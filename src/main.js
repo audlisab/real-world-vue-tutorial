adb0476c2c28f32b0d60950acfe1f097f08eea8d
lizaveta <lizaveta.prakapovich@stazysta.comarch.pl>
2020-08-27 11:41:36 +0200

    First Commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaseIcon from './components/BaseIcon.vue'
+
+Vue.config.productionTip = false
+
+Vue.component('BaseIcon', BaseIcon)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Event listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '../views/EventCard.vue'
+import EventService from "../services/EventService";
+
+export default {
+  components: {
+    EventCard
+  },
+  created() {
+    EventService
+      .getEvents()
+      .then(response => {
+        console.log(response.data)
+        this.events = response.data
+      })
+      .catch(errors => {
+        console.log(errors)
+      })
+  },
+  data() {
+    return {
+      events: []
+    }
+  }
+}
+</script>
