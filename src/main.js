24f28e51b6c521ece632395e0e26b7fdf70ad6e0
lizaveta <lizaveta.prakapovich@stazysta.comarch.pl>
2020-08-31 15:56:47 +0200

    Enabled global registration

--- src/main.js
+++ src/main.js
@@ -5,23 +5,31 @@ import store from './store'
 import router from './router/router'
 import 'nprogress/nprogress.css'
 
-import BaseIcon from './components/BaseIcon.vue'
-import BaseInput from './components/BaseInput'
-import BaseSelect from './components/BaseSelect'
-import BaseButton from './components/BaseButton'
 import DateFilter from './filters/date'
 import Vuelidate from 'vuelidate/src'
+import {camelCase, upperFirst} from "lodash";
 
 Vue.config.productionTip = false
-
-Vue.component('BaseIcon', BaseIcon)
-Vue.component('BaseInput', BaseInput)
-Vue.component('BaseSelect', BaseSelect)
-Vue.component("BaseButton", BaseButton)
 
 Vue.filter('date', DateFilter)
 
 Vue.use(Vuelidate)
+
+const requireComponent = require.context(
+    './components',
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+      camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 new Vue({
   router,
